911cf1ae56747f938099e0f8804aec5133c10912
brzera00 <brsvmoreira@gmail.com>
2024-12-26 14:51:09 -0300

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,17 +29,19 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/Vu3wBmAKPP";
+      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
       const githubLink = "https://github.com/GlaceYT";
+      const replitLink = "https://replit.com/@GlaceYT";
+      const youtubeLink = "https://www.youtube.com/@GlaceYT";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230822430973628496/1297599151462285333/7219-victor-wang-pfpsgg.gif?ex=676ed761&is=676d85e1&hm=ab0b52e59830849274dafa2e99ef38f0270384c39948d5706450294a715790b3&', 
-              url: 'https://discord.gg/Vu3wBmAKPP'
+              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n
-            .setImage('https://cdn.discordapp.com/attachments/1230822430973628496/1297599151462285333/7219-victor-wang-pfpsgg.gif?ex=676ed761&is=676d85e1&hm=ab0b52e59830849274dafa2e99ef38f0270384c39948d5706450294a715790b3&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
